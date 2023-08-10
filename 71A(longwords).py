d0ffe51591d2c83a9d4a089a64900b2cd83b713e
Ilhomjon Rahimov <mynameisilhom@gmail.com>
2023-08-10 16:28:27 +0500

    updated

---
+++ 71A(longwords).py
@@ -0,0 +1,10 @@
+words = ['word', 'localization', 'internationalization', 'pneumonoultramicroscopicsilicovolcanoconiosis']
+
+first = [word[0] for word in words]
+last = [word[-1] for word in words]
+
+
+i = first+last
+
+# print(words[0])
+print(i)
